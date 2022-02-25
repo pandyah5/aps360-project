4b90e717d4b77d3720d67fab8cf03350d7699696
Yifan Lu <lyf19990329@me.com>
2022-02-25 11:55:02 -0500

    Fixed a grammatical error.

--- baseline/utils/cv2_bbox.py
+++ baseline/utils/cv2_bbox.py
@@ -4,12 +4,12 @@ import cv2
 def get_bbox(image, threshold, vis=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     if vis:
-        cv2.imshow(gray)
+        cv2.imshow("gray", gray)
 
     canny_output = cv2.Canny(gray, threshold, threshold * 2)
 
     if vis:
-        cv2.imshow(canny_output)
+        cv2.imshow("canny", canny_output)
 
     contours, hierarchy = cv2.findContours(canny_output, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     print("Number of contours:" + str(len(contours)))
@@ -20,4 +20,4 @@ def get_bbox(image, threshold, vis=False):
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
     if vis:
-        cv2.imshow(image)
+        cv2.imshow("bounding box", image)
